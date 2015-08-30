ede8d4ebc1f89a5a5e42d9b8dab2ec5faf09fa51
Geoff Viola <geoffrey.viola@asirobots.com>
2015-08-30 12:39:47 -0600

    removed some clang warnings

--- Source/Libraries/Asi/Wrappers/GTest.hpp
+++ Source/Libraries/Asi/Wrappers/GTest.hpp
@@ -3,8 +3,15 @@
 
 #ifdef __GNUC__
 #pragma GCC diagnostic ignored "-Wconversion-null"
+#elseif __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wused-but-marked-unused"
 #endif
 
 #include "gtest/gtest.h"
+
+#if __clang__
+#pragma clang diagnostic pop
+#endif
 
 #endif //ASI_GTEST_HPP
